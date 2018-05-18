8cb4e7adc8ba52401bd0e3706132a5ae3080a8a9
ymy <649048652@qq.com>
2018-05-18 11:27:01 +0800

    啦啦啦来ala了

--- Qingcheng/src/main/java/nsu/edu/qingcheng/controller/Test.java
+++ Qingcheng/src/main/java/nsu/edu/qingcheng/controller/Test.java
@@ -2,6 +2,7 @@ package nsu.edu.qingcheng.controller;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("这是一个测试");
+		System.out.println("这是一个测试"); 
+		System.out.println("这是二个测试");
 	}
 }
